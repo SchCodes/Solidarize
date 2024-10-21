b28a24b8a4ddb491df3359f9b294eaf491076d59
SchCodes <schcodes@gmail.com>
2024-10-20 21:24:09 -0300

    base.html e css / header

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.render_index_home, name='render_index_home'),
+    path('', views.home, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def render_index_home(request):
+def home(request):
     return render(request, "home/html/index.html")
